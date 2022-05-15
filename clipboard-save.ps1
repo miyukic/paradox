0e6ffc417a71e2f6f370489b5437085882d23157
miyukic <m.yukic10@gmail.com>
2022-05-15 23:54:15 +0900

    update

--- clipboard-save.ps1
+++ clipboard-save.ps1
@@ -1,12 +1,19 @@
 #保存ファイル名
 $SaveImgeFile = 'ImageFile.png'
+$CurrentDir = Convert-Path .
+$DesktopPath = [System.Environment]::GetFolderPath("Desktop")
+if ($CurrentDir -match "^C:\\Program[ \t]Files\\PowerShell\\[0-9]+") {
+    $CurrentDir = $DesktopPath
+}
 
 if ((Get-Host).Version.Major -le 5) {   #WindowsPowerShell
-    (Get-Clipboard -Format Image).Save($SaveImgeFile)
-
-} elseif ((Get-Host).Version.Major -gt 6) {
+        (Get-Clipboard -Format Image).Save($CurrentDir + "\" + $SaveImgeFile)
+    echo ($SaveImgeFile + " 名で保存されました。\n" + "Windows PowerShell")
+} elseif ((Get-Host).Version.Major -le 7) {
     Add-Type -AssemblyName System.Windows.Forms
-    $DesktopPath = [System.Environment]::GetFolderPath("Desktop")
     $clip = [Windows.Forms.Clipboard]::GetImage()
-    if ($null -ne $clip) { $clip.Save($DesktopPath + '\' + $SaveImgeFile) }
+    if ($null -ne $clip) {
+        $clip.Save($CurrentDir + "\" + $SaveImgeFile)
+    }
 }
+#pause
