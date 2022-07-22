beb7d61a637f922b2a782c4f782d21f14caf5c0b
miyukic <m.yukic10@gmail.com>
2022-07-22 22:25:59 +0900

    add: GetActiveObject pwsh向け再実装

---
+++ GetActiveObject.ps1
@@ -0,0 +1,47 @@
+# 使い方(How to use)
+# #$EXCEL = [Marshall2]::GetActiveObject("Excel.Application")
+
+Add-Type -TypeDefinition @'
+using System;
+using System.Runtime;
+using System.Runtime.InteropServices;
+public static class Marshal2
+{
+    internal const String OLEAUT32 = "oleaut32.dll";
+    internal const String OLE32 = "ole32.dll";
+
+    public static Object GetActiveObject(String progID)
+    {
+        Object obj = null;
+        Guid clsid;
+
+        // Call CLSIDFromProgIDEx first then fall back on CLSIDFromProgID if
+        // CLSIDFromProgIDEx doesn't exist.
+        try
+        {
+            CLSIDFromProgIDEx(progID, out clsid);
+        }
+        //            catch
+        catch (Exception)
+        {
+            CLSIDFromProgID(progID, out clsid);
+        }
+
+        GetActiveObject(ref clsid, IntPtr.Zero, out obj);
+        return obj;
+    }
+
+    //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+    [DllImport(OLE32, PreserveSig = false)]
+    private static extern void CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
+
+    //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+    [DllImport(OLE32, PreserveSig = false)]
+    private static extern void CLSIDFromProgID([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
+
+    //[DllImport(Microsoft.Win32.Win32Native.OLEAUT32, PreserveSig = false)]
+    [DllImport(OLEAUT32, PreserveSig = false)]
+    private static extern void GetActiveObject(ref Guid rclsid, IntPtr reserved, [MarshalAs(UnmanagedType.Interface)] out Object ppunk);
+
+}
+'@
